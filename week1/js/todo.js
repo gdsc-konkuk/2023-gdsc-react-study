e68a0b694cbfa46c31a69b2b131ce4f37ec7bea3
xoxun <00xxs2@naver.com>
2023-11-07 02:56:19 +0900

    feat: addTodo

--- week1/js/todo.js
+++ week1/js/todo.js
@@ -1,3 +1,36 @@
 const header = document.querySelector(".header");
+const todoForm = document.querySelector(".header__input-form");
+const todoInput = document.querySelector(".header__input-form input");
 const todo = document.querySelector(".todo");
+const todoList = document.querySelector(".todo__list");
 const done = document.querySelector(".done");
+
+const deleteTodo = (event) => {
+
+}
+
+const addTodo = (newTodo) => {
+    const li = document.createElement("li");
+    li.innerText = newTodo;
+    const deleteButton = document.createElement("i");
+    deleteButton.className = "fa-solid fa-delete-left"
+    todoList.appendChild(li);
+    todoList.appendChild(deleteButton);
+
+
+
+}
+
+const handleTodoSubmit = (event)=>{
+    event.preventDefault();
+    const newTodo = todoInput.value;
+    todoInput.value = "";
+    console.log(newTodo);
+
+    addTodo(newTodo);
+}
+
+todoForm.addEventListener("submit", handleTodoSubmit);
+
+
+console.log("Asdfd");
