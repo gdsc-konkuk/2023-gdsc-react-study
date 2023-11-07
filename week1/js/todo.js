e04492873de45d64d09477faae8476d0dda4d959
xoxun <00xxs2@naver.com>
2023-11-07 20:38:33 +0900

    feat: deleteTodo, saveTodo

--- week1/js/todo.js
+++ week1/js/todo.js
@@ -4,33 +4,79 @@ const todoInput = document.querySelector(".header__input-form input");
 const todo = document.querySelector(".todo");
 const todoList = document.querySelector(".todo__list");
 const done = document.querySelector(".done");
+const doneList = document.querySelector(".done__list");
 
-const deleteTodo = (event) => {
+const TODOS_KET = "TODOS"; //
 
+let todos = [];
+
+const saveTodos = () => {
+    localStorage.setItem(TODOS_KET, JSON.stringify(todos));
+}
+
+function deleteTodo(event){
+    event.preventDefault();
+    const thisLi = this.closest("li"); //부모 찾기
+    const thisSpan = this.previousSibling; //이전 형제 찾기 
+    todos = todos.filter(item => item !== thisSpan.innerText);
+    thisLi.remove();
+    saveTodos();
+}
+
+function doneTodo(event){
+    event.preventDefault();
+    const thisLi = this.closest("li"); //부모찾기
+    thisLi.className = "middle-line";
+    doneList.appendChild(thisLi);
+    
 }
 
 const addTodo = (newTodo) => {
+    const span = document.createElement("span");
     const li = document.createElement("li");
-    li.innerText = newTodo;
+    span.innerText = newTodo;
     const deleteButton = document.createElement("i");
     deleteButton.className = "fa-solid fa-delete-left"
+    li.appendChild(span);
+    li.appendChild(deleteButton);
     todoList.appendChild(li);
-    todoList.appendChild(deleteButton);
 
-
-
+    span.addEventListener("click", doneTodo);
+    deleteButton.addEventListener("click", deleteTodo);
 }
+
+function isDuplicate(arr)  {
+    const isDup = arr.some(function(x) {
+      return arr.indexOf(x) !== arr.lastIndexOf(x);
+    });
+                           
+    return isDup;
+  }
 
 const handleTodoSubmit = (event)=>{
     event.preventDefault();
     const newTodo = todoInput.value;
     todoInput.value = "";
-    console.log(newTodo);
+    console.log(todos);
 
-    addTodo(newTodo);
+    const tempTodos = [...todos];
+    tempTodos.push(newTodo);
+    if(!isDuplicate(tempTodos)) {
+        todos.push(newTodo);
+        addTodo(newTodo);
+    }
+    saveTodos();
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
 
+const savedTodos = localStorage.getItem(TODOS_KET);
+if(savedTodos != null){
+    const parsedTodos = JSON.parse(savedTodos);
+    todos = parsedTodos;
 
-console.log("Asdfd");
+    parsedTodos.forEach((item) => {
+        console.log(`this is the turn of ${item.text}`);
+        addTodo(item);
+    });
+}
